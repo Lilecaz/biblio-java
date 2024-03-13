169faa70c197024250fd94ee973ca23a2a06af62
themanajaa <themanajaa@gmail.com>
2024-03-13 13:16:23 +0100

    liste déroulante ajouté

--- src/main/java/org/example/biblio_projet_java/MainApp.java
+++ src/main/java/org/example/biblio_projet_java/MainApp.java
@@ -2,24 +2,43 @@ package org.example.biblio_projet_java;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> System.out.println("Hello World!"));
+        // Création des éléments de menu
+        MenuItem menuItem1 = new MenuItem("Ouvrir");
+        MenuItem menuItem2 = new MenuItem("Quitter");
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        // Création du menu
+        Menu menu = new Menu("Fichier");
+        menu.getItems().addAll(menuItem1, menuItem2);
 
+        Menu menu2 = new Menu("Edition");
+        menu.getItems().addAll();
+
+        Menu menu3 = new Menu("About");
+        menu.getItems().addAll();
+
+        // Création de la barre de menu et ajout du menu
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(menu, menu2, menu3);
+
+        // Création de la mise en page principale
+        VBox root = new VBox();
+        root.getChildren().add(menuBar);
+
+        // Création de la scène
         Scene scene = new Scene(root, 300, 250);
 
-        primaryStage.setTitle("Hello World!");
+        // Configuration de la scène et affichage de la fenêtre
+        primaryStage.setTitle("Biblio");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
