866ecc8969c484b216437ac3294e2f3f578e0b9f
lilecaz <lilecaz0090@gmail.com>
2024-03-17 18:08:33 +0100

    ajout de l'import du fichier XML

--- src/main/java/org/example/biblio_projet_java/MainApp.java
+++ src/main/java/org/example/biblio_projet_java/MainApp.java
@@ -2,12 +2,16 @@ package org.example.biblio_projet_java;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.example.biblio_projet_java.Bibliotheque;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 
 public class MainApp extends Application {
 
@@ -18,7 +22,19 @@ public class MainApp extends Application {
         FormulaireLivre formulaireLivre = new FormulaireLivre(tableView);
 
         MenuItem menuItem1 = new MenuItem("Ouvrir");
+        menuItem1.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Ouvrir un fichier XML");
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Fichiers XML", "*.xml"));
+            File selectedFile = fileChooser.showOpenDialog(primaryStage);
+            if (selectedFile != null) {
+                chargerFichierXML(selectedFile, tableView);
+            }
+        });
+
         MenuItem menuItem2 = new MenuItem("Quitter");
+        menuItem2.setOnAction(event -> primaryStage.close());
 
         MenuItem menuItem3 = new MenuItem("Sauvegarder");
         MenuItem menuItem4 = new MenuItem("Sauvegarder sous...");
@@ -41,13 +57,26 @@ public class MainApp extends Application {
         root.getChildren().addAll(
                 menuBar,
                 formulaireLivre,
-                tableView.getTableView());
+                tableView);
 
         Scene scene = new Scene(root, 600, 400);
 
         primaryStage.setTitle("Biblio");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void chargerFichierXML(File file, LivreTableView tableView) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Bibliotheque.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            Bibliotheque bibliotheque = (Bibliotheque) unmarshaller.unmarshal(file);
+            bibliotheque.getLivre().forEach(tableView::ajouterLivre);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Erreur lors du chargement du fichier XML.");
+            alert.showAndWait();
+        }
     }
 
     public static void main(String[] args) {
