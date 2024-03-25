84778132cb0f29a782b8b13749d48ef3e79c81bb
Lilecaz <lilecaz0090@gmail.com>
2024-03-25 10:08:53 +0100

    test jar executable

---
+++ src/main/java/org/example/biblio_projet_java/NewMain.java
@@ -0,0 +1,9 @@
+package org.example.biblio_projet_java;
+
+import org.example.biblio_projet_java.MainApp;
+public class NewMain {
+
+  public static void main(String[] args) {
+      MainApp.main(args);
+  }
+}
