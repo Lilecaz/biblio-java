08008cd6d410b80753d3a0073d975b1b7ddc3265
Lilecaz <lilecaz0090@gmail.com>
2024-03-25 10:26:49 +0100

    jar exec

--- src/main/java/org/example/biblio_projet_java/NewMain.java
+++ src/main/java/org/example/biblio_projet_java/NewMain.java
@@ -1,9 +1,8 @@
 package org.example.biblio_projet_java;
 
-import org.example.biblio_projet_java.MainApp;
 public class NewMain {
 
   public static void main(String[] args) {
-      MainApp.main(args);
+    MainApp.main(args);
   }
 }
