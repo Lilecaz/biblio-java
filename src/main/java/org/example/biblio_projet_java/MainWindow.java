55c5c5eecfc4c37718a0613d82446b3d8d85f81b
Lilecaz <lilecaz0090@gmail.com>
2024-06-13 12:14:59 +0200

    fix bug

--- src/main/java/org/example/biblio_projet_java/MainWindow.java
+++ src/main/java/org/example/biblio_projet_java/MainWindow.java
@@ -287,7 +287,7 @@ public class MainWindow extends Application {
             String usernameText = credentials.getKey();
             String passwordText = credentials.getValue();
             try {
-                if (database.registerUser(usernameText, passwordText)) {
+                if (databaseManager.registerUser(usernameText, passwordText)) {
                     showAlert(Alert.AlertType.INFORMATION, "Inscription réussie", "Vous êtes maintenant inscrit.");
                 } else {
                     showAlert(Alert.AlertType.ERROR, "Échec de l'inscription", "L'inscription a échoué.");
