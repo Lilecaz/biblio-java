cda6cea0a8f4b947cdf5c3f644ea4a155ea4cc37
Lilecaz <lilecaz0090@gmail.com>
2024-06-26 14:58:41 +0200

    testdevops

--- src/main/java/org/example/biblio_projet_java/main/Main.java
+++ src/main/java/org/example/biblio_projet_java/main/Main.java
@@ -3,7 +3,7 @@ package org.example.biblio_projet_java.main;
 import org.example.biblio_projet_java.view.MainWindow;
 
 public class Main {
-
+//test devops
   public static void main(String[] args) {
     MainWindow.main(args);
   }
